3c9d27ea208373bb29fb4bcea0dcced721c2fdbe
MiaDobler <113926918+MiaDobler@users.noreply.github.com>
2023-10-12 00:37:24 -0400

    Add files via upload

--- LotR API - 2910 Lab 5.py
+++ LotR API - 2910 Lab 5.py
@@ -5,9 +5,43 @@ import json
 #key = kxTkKFL3JRyaIDDoLknv 
 
 
-userInput = input("What character do you want: ")
+#using https://www.youtube.com/watch?v=63nw00JqHo0 as resource
+def menu():
+    print("Menu for The One API Implementation \n--------------------\n")
+    print("[1] Look up a Character")
+    print("[0] Exit Program")
 
-#some initial issues with the API accessing and how to do it with a toke, tried different methods, settled on this:
+menu()
+
+option = int(input("Enter option choice: "))
+while option!=0:
+    if option == 1:
+        #currently repeats the following in a loop when option one has been called
+        userInput = input("What character do you want: ")
+        API_URL = f"https://the-one-api.dev/v2/character?name={userInput}"
+        headers = {
+        'Authorization': 'Bearer kxTkKFL3JRyaIDDoLknv',
+        }
+        response = requests.get(API_URL, headers=headers)
+
+        if response.status_code == 200:
+            data = response.json() 
+        else:
+            print(f"Failed to retrieve data. Status code: {response.status_code}")
+            data = None
+
+        print (response.json())
+
+    else:
+        print("Invalid option.")
+
+
+
+
+
+
+
+#some initial issues with the API accessing and how to do it with a token, tried different methods, settled on this:
 API_URL = f"https://the-one-api.dev/v2/character?name={userInput}"
 headers = {
     'Authorization': 'Bearer kxTkKFL3JRyaIDDoLknv',
@@ -23,21 +57,4 @@ else:
 
 print (response.json())
 
-print("\n" +
-                      "Menu for The One API Implementation \n" +
-                      "--------------------\n" +
-                      "[1] Get a quote\n" +
-                      "[2] Look up Character\n" +
-                      "[3] End the Program\n" +
-                      "\n")
-userinputt = ("Please enter the number of your choice: ")
 
-def number_to_string(userinputt):
-    match argument:
-        case 0:
-            return "zero"
-        case 1:
-            return "one"
-        case 2:
-            return "two"
-        case default:
